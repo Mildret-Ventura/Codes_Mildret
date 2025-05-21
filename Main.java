28c523107263cfc06bf26c0246163429d1a6534c
Mildret-Ventura <20243ds047@utez.edu.mx>
2025-05-21 08:52:42 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main {
         String numero1 = JOptionPane.showInputDialog("Dame el primer promedio");
         String numero2 = JOptionPane.showInputDialog("Dame segundo promedio");
         String numero3 = JOptionPane.showInputDialog("Dame segundo promedio");
-        System.out.println((Integer.parseInt(numero1) + Integer.parseInt(numero2) + Integer.parseInt(numero3))/3);
+        System.out.println((Float.parseFloat(numero1) + Float.parseFloat(numero2) + Float.parseFloat(numero3))/3); 
     }
-
 }
