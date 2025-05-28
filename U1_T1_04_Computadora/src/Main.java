bec2d3f05b4eaeeac7faf5a8f9bca330fd797c5c
Mildret-Ventura <20243ds047@utez.edu.mx>
2025-05-28 08:46:12 -0600

    Primer commit

---
+++ U1_T1_04_Computadora/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Computadora miComputadora = new Computadora("Lenovo", "ThinkPad X1");
+
+        miComputadora.encender();
+        miComputadora.encender();
+        miComputadora.apagar();
+    }
+}
